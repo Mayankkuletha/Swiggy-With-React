5a24163e509b193fd9122c139f2b587c73033838
Mayank Kuletha <mayankkuletha512@gmail.com>
2026-03-23 15:49:54 +0530

    updated swiggy clone - cart and UI fixes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import Restaurant from "./components/Restaurant";
 import RestaurantMenu from "./components/RestaurantMenu";
 import {BrowserRouter ,Routes , Route } from "react-router";
 import SearchFood from "./components/SearchFood";
+import SecondaryHome from "./components/SecondaryHome";
+import { store } from "./stores/stores";
+import {Provider} from "react-redux";
+import Checkout from "./components/Checkout";
 function App() {
     return (
         <>
+        <Provider store={store}>
         <BrowserRouter>
         <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route element={<SecondaryHome></SecondaryHome>}>
         <Route path="/restaurants" element={<Restaurant></Restaurant>}></Route>
         <Route path="/city/delhi/:id" element={<RestaurantMenu></RestaurantMenu>}></Route>
         <Route path="/city/delhi/:id/search" element={<SearchFood></SearchFood>}></Route>
+        </Route>
+        <Route path="/Checkout" element={<Checkout></Checkout>}></Route>
         </Routes>
 
         </BrowserRouter>
-       
+        </Provider>
         </>
        
     )
--- src/components/RestInfo.js
+++ src/components/RestInfo.js
@@ -1,23 +1,49 @@
-export default function RestInfo ({restData}){
-    return(
+import { useState } from "react";
+import { addItems, IncrementItems, DecrementItems } from "../stores/CartSlicer";
+import { useDispatch, useSelector } from "react-redux";
+export default function RestInfo({ restData }) {
+    const dispatch = useDispatch();
+    const items = useSelector(state => state.cartslice.items);
+    const element = items.find(item => item.id === restData.id);
+    const count = element ?element.quantity:0;
+    function handleAddItems() {
+
+        dispatch(addItems(restData));
+    }
+
+    function handleIncrementItems() {
+
+        dispatch(IncrementItems(restData));
+    }
+    function handleDecrementItems() {
+        ;
+        dispatch(DecrementItems(restData));
+    }
+    return (
         <>
-        
-        <div className="w-full flex justify-between mb-2 pb-2">
-            <div className="w-[70%]">
-            <p className="text-2xl text-gray-700 font-semibold mb-1">{restData?.name}</p>
-            <p className="text-xl">{"₹"+("defaultPrice" in restData ? restData?.defaultPrice/100:restData?.price/100)}</p>
-            <span className="text-green-700">{restData?.rating?.aggregatedRating?.rating}</span>
-            <span>{"("+restData?.rating?.aggregatedRating?.ratingCountV2+")"}</span>
-            <p>
-                {restData.description}
-            </p>
+            <div className="w-full flex justify-between mb-2 pb-2">
+                <div className="w-[70%]">
+                    <p className="text-2xl text-gray-700 font-semibold mb-1">{restData?.name}</p>
+                    <p className="text-xl">{"₹" + ("defaultPrice" in restData ? restData?.defaultPrice / 100 : restData?.price / 100)}</p>
+                    <span className="text-green-700">{restData?.rating?.aggregatedRating?.rating}</span>
+                    <span>{"(" + restData?.rating?.aggregatedRating?.ratingCountV2 + ")"}</span>
+                    <p>
+                        {restData.description}
+                    </p>
+                </div>
+                <div className="w-[20%] relative h-42">
+                    <img className="w-60 h-36 object-cover rounded-2xl" src={"https://media-assets.swiggy.com/swiggy/image/upload/" + restData?.imageId}></img>
+                    {
+                        (count === 0) ? (<button className="absolute bottom-1 left-20 rounded-xl text-xl  text-green-600 px-6 py-2 shadow-md border border-white bg-white" onClick={() => handleAddItems()}>ADD</button>) : (<div className="absolute bottom-1 left-20 flex gap-3 text-2xl rounded-xl  text-green-600 px-6 py-2 shadow-md border border-white bg-white">
+                            <button onClick={() => handleDecrementItems()}>-</button>
+                            <span>{count}</span>
+                            <button onClick={() => handleIncrementItems()}>+</button>
+                        </div>)
+                    }
+
+                </div>
             </div>
-            <div className="w-[20%] relative">
-            <img className="w-full h-36 object-cover rounded-2xl" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+restData?.imageId}></img>
-            <button className="absolute bottom-1 left-20 rounded-xl text-xl  text-green-600 px-6 py-2 shadow-md border border-white bg-white">ADD</button>
-            </div>
-        </div>
-        <hr className="mb-6 mt-2 "></hr>
+            <hr className="mb-6 mt-2 "></hr>
         </>
     )
 }
